3aec5de8cb1549dc20a46600504831c00f893cbd
Juan Sarmiento <juanchosarmiento.97@gmail.com>
2025-03-12 10:06:48 -0500

    prueba

--- obj/Debug/net9.0/JuanSarmientoApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/JuanSarmientoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JuanSarmientoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90ae68fc76119138409433be5093db995c336631")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+055c286b084f02d864d599a79d3e0b75ad8237a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("JuanSarmientoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JuanSarmientoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
